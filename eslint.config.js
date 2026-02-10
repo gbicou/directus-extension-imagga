8616274eeecc2cae300c86fced3d1fe19fcc3be8
Benjamin VIELLARD <gbicou@gmail.com>
2026-02-10 07:00:04 +0100

    chore: update ESLint configuration to use `defineConfig` and adjust imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,17 @@
 // @ts-check
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+import js from '@eslint/js'
+import ts from 'typescript-eslint'
 import stylistic from '@stylistic/eslint-plugin'
 import unicorn from 'eslint-plugin-unicorn'
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['dist/'],
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.stylistic,
-  stylistic.configs['recommended-flat'],
-  unicorn.configs['flat/recommended'],
+  js.configs.recommended,
+  ts.configs.recommended,
+  ts.configs.stylistic,
+  stylistic.configs.recommended,
+  unicorn.configs.recommended,
 )
